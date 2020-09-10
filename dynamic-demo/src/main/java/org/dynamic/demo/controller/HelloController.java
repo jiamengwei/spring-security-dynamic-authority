5f2eef4a476c5e25b940d3aaf0b578e89a6933a3
jiamengwei <jia0109@outlook.com>
2020-09-10 11:50:47 +0800

    '删除冗余代码'

--- dynamic-demo/src/main/java/org/dynamic/demo/controller/HelloController.java
+++ dynamic-demo/src/main/java/org/dynamic/demo/controller/HelloController.java
@@ -1,9 +1,6 @@
 package org.dynamic.demo.controller;
 
 import org.dynamic.demo.entity.TUser;
-import org.dynamic.demo.events.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @Autowired
-    private EmailService emailService;
-
     @GetMapping("")
     public String hello(TUser tUser) {
-        emailService.sendEmail("beijing", "haha");
         return "Hello ! ! !" + tUser.toString();
     }
 
